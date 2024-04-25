dbe6efa9940b63b38e7687fab6bb7016a7cc44f8
RenOoishi <o-ren-2409@occ.co.jp>
2024-04-25 12:30:56 +0900

    Support Me on Ko-fiを追加

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,12 +1,8 @@
 import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const nunito = Nunito({ 
-  weight: ["400", "500"],
-  subsets: ["latin"],
-  display: "swap" 
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={nunito.className}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
